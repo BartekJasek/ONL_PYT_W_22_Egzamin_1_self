78fe474779c7d451abc5de221d040403178660bc
BartekJasek <bartek.jasek5@gmail.com>
2022-11-26 10:45:38 +0100

    zadanie1

--- 01_Zadanie_1/answer1.py
+++ 01_Zadanie_1/answer1.py
@@ -1,8 +1,13 @@
 def check_character(word, letter):
-    if letter in word:
-        print("Character found")
-    else:
-        print("Character not found")
+    answer = []
+    for word in word.split():
+        for sign in word:
+            if sign == letter:
+                answer.append(1)
+    return len(answer)
 
 
 print(check_character('Order of the Phoenix', 'o'))
+print(check_character('Order of the Phoenix', 'z'))
+print(check_character('Order of the Phoenix', 'p'))
+print(check_character('Order of the Phoenix', 'P'))
